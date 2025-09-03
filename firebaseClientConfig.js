4043816f762d368f5e0609756f390d7e48349b51
anabmguimaraes <anabm.guimaraes@gmail.com>
2025-09-02 23:57:10 -0300

    Update firebaseClientConfig.js

--- firebaseClientConfig.js
+++ firebaseClientConfig.js
@@ -3,21 +3,22 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Adicione a configuração do seu projeto Firebase aqui
-// Você pode encontrar isso no console do Firebase:
-// Configurações do projeto > Geral > Seus apps > App da Web
+// Sua configuração do app da web do Firebase, fornecida pelo console
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyCNU5ZEl60OcW5eZyL_ZoD0tFKpweQvhwU",
+  authDomain: "crmdoceria-9959e.firebaseapp.com",
+  projectId: "crmdoceria-9959e",
+  storageBucket: "crmdoceria-9959e.firebasestorage.app",
+  messagingSenderId: "389481198252",
+  appId: "1:389481198252:web:429bff3cc5d4f353bea509",
+  measurementId: "G-XJ7LPG0229"
 };
 
 // Inicialize o Firebase
 const app = initializeApp(firebaseConfig);
 
 // Exporte os serviços que você usará na sua aplicação
+// Nós precisamos do Auth (para login) e do Firestore (para salvar as permissões)
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
